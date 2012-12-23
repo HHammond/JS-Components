2b75d855844a72906f5514139c718e08302e1f0d
Henry Hammond <HenryHHammond92@gmail.com>
2012-12-23 13:11:42 -0500

    Created base structure for project

---
+++ slideshow.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+
+}); 
+
+// slideshow element click
+
+// mask content
+
+// display lightbox
+
+//load (full) image into box
+
+//load images in directory (xml data)
